56aeb36c902c2e324fe5e1873fca70a2f571bb13
muathendirangu <muathe.ndirangu@gmail.com>
2023-10-15 15:10:23 +0300

    model database methods for the contacts data

---
+++ src/contact/contacts.sql.ts
@@ -0,0 +1,79 @@
+/**
+ * import the required external modules
+ */
+import {
+    db
+} from './db.config';
+/**
+ *
+ * import data model interfaces
+ *
+ */
+import {
+    Contact
+} from './contact.interface';
+
+
+export const getContacts = async (): Promise < Contact[] > => {
+    return new Promise < Contact[] > ((resolve, reject) => {
+        db.all('SELECT * FROM contacts', (err, rows: Contact[]) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(rows);
+            }
+        });
+    });
+}
+export const getContact = async (id: number): Promise < Contact | null > => {
+    return new Promise < Contact | null > ((resolve, reject) => {
+        db.get('SELECT * FROM contacts WHERE id = ?', [id], (err, row: Contact | null) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(row);
+            }
+        });
+    });
+}
+export const insertContact = async (contact: Contact): Promise < Contact > => {
+    return new Promise < Contact > ((resolve, reject) => {
+        db.run('INSERT INTO contacts (name, phoneNumber) VALUES (?, ?)',
+            [contact.name, contact.phoneNumber],
+            function(err) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(contact);
+                }
+            });
+    });
+}
+export const updateContactInfo = async (id: number, updatedContact: Contact): Promise < Contact | null > => {
+    return new Promise < Contact | null > ((resolve, reject) => {
+        db.run('UPDATE contacts SET name = ?, phoneNumber = ? WHERE id = ?',
+            [updatedContact.name, updatedContact.phoneNumber, id],
+            function(err) {
+                if (err) {
+                    reject(err);
+                } else if (this.changes === 0) {
+                    resolve(null);
+                } else {
+                    resolve(updatedContact);
+                }
+            });
+    });
+}
+export const deleteContact = async (id: number): Promise < void | null > => {
+    return new Promise < void | null > ((resolve, reject) => {
+        db.run('DELETE FROM contacts WHERE id = ?', [id], function(err) {
+            if (err) {
+                reject(err);
+            } else if (this.changes === 0) {
+                resolve(null);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
