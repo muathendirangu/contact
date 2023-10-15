db2dd785a0fb990e37173bbcd8378a4dc7845aa9
muathendirangu <muathe.ndirangu@gmail.com>
2023-10-15 18:01:04 +0300

    update id type on service methods

--- src/contact/contacts.service.ts
+++ src/contact/contacts.service.ts
@@ -3,6 +3,7 @@
  * import data model interfaces
  *
  */
+import { UUID } from 'crypto';
 import {
   Contact
 } from './contact.interface';
@@ -21,15 +22,15 @@ import {
 export const findAllContacts = async (): Promise < Contact[] > => {
   return await getContacts();
 }
-export const findContact = async (id: number): Promise < Contact | null > => {
+export const findContact = async (id: string): Promise < Contact | null > => {
   return await getContact(id);
 }
 export const createContact = async (newContact: Contact): Promise < Contact > => {
   return await insertContact(newContact);
 }
-export const updateContact = async (id: number, updatedContact: Contact): Promise < Contact | null > => {
+export const updateContact = async (id: string, updatedContact: Contact): Promise < Contact | null > => {
   return await updateContactInfo(id, updatedContact);
 }
-export const removeContact = async (id: number): Promise < null | void > => {
+export const removeContact = async (id: string): Promise < null | void > => {
   return await deleteContact(id);
 }
