d52c79f1d786e76e793f6fe79e67f6554f922fef
muathendirangu <muathe.ndirangu@gmail.com>
2023-10-15 21:16:45 +0300

    add type guard for input parameters for encrypt and decrypt functions and add docstrings to encryption class

--- src/middleware/encryption.middleware.ts
+++ src/middleware/encryption.middleware.ts
@@ -16,42 +16,48 @@ export default class Encryption {
         // encode encryption key from utf8 to hex
         const ENCRYPTION_KEY = config.encryptionKey ? Buffer.from(config.encryptionKey).toString('hex') : '';
         // initialize key
-        this.key = ENCRYPTION_KEY ? Buffer.from(ENCRYPTION_KEY, "hex") : '';
+        this.key = ENCRYPTION_KEY ? Buffer.from(ENCRYPTION_KEY, 'hex') : '';
         // initialize IV
         this.iv = config.iv || null;
-        // validate missing config options
-        if (!this.algorithm && !this.key) {
-            throw Error('There was an issue with configuration!');
+        // Validate missing config options
+        if (!this.algorithm || !this.key) {
+            throw new Error('There was an issue with configuration!');
         }
     }
     /**
-     * Function to encrypt data
+     * Encrypts data.
+     *
+     * @param data The data to encrypt.
+     * @returns The encrypted data.
      */
-    encrypt = (data: any): string => {
+    encrypt(data: string): string {
         // Validate missing data
         if (!data) {
-            throw Error('data to encrypt is required!');
+            throw new Error('Data to encrypt is required!');
         }
         // Initialize Cipher instance
         const cipher = createCipheriv(this.algorithm, this.key, this.iv);
         // Get encrypted data from the cipher instance
-        const encryptedData = cipher.update(data, "utf8", "hex") + cipher.final("hex");
+        const encryptedData = cipher.update(data, 'utf8', 'hex') + cipher.final('hex');
         // Return encrypted data
         return encryptedData;
     }
     /**
-     * Function to decrypt data
+     * Decrypts data.
+     *
+     * @param data The data to decrypt.
+     * @returns The decrypted data.
      */
-    decrypt = (data: any): string => {
+    decrypt(data: string): string {
         // Validate missing data
         if (!data) {
-            throw Error('Data to decrypt is required!');
+            throw new Error('Data to decrypt is required!');
         }
         // Initialize Decipher instance
         const decipher = createDecipheriv(this.algorithm, this.key, this.iv);
         // Get decrypted data from decipher instance
         const decryptedData = decipher.update(data, 'hex', 'utf8') + decipher.final('utf8');
-        // return decrypted data
+        // Return decrypted data
         return decryptedData;
     }
 }
